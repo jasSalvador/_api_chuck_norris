30abaa14f6b77865188b257de87ddeca49f25083
Jasmin Salvador <sjasmin.salvador@gmail.com>
2024-10-15 19:43:16 -0300

    agregando funcion fraseInicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 let btnFrase = document.getElementById("btnFrase");
-let frasechuck = document.getElementById("fraseChuck");
+let fraseChuck = document.getElementById("fraseChuck");
 
 const obtenerFrase = async() => {
     try {
         const response = await fetch('https://api.chucknorris.io/jokes/random');
         const result = await response.json();
         console.log(result);
-        frasechuck.innerHTML = result.value;
+        fraseChuck.innerHTML = result.value;
         } catch (error) {
             console.log(error);
         }
 }
 
 // obtenerFrase();
-
 btnFrase.addEventListener('click', obtenerFrase);
+
+
+const freseInicio = async() => {
+    try {
+        let response = await obtenerFrase();
+        fraseChuck.innerHTML = response.value;
+        } catch (error) {
+    }
+};
+
+freseInicio();
